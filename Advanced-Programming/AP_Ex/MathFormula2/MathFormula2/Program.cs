80270530df31dbff16fd2f46f94c3c2dde1047fc
sinanazem <sinanaazem@gmail.com>
2023-11-02 08:32:27 +0330

    add math-formula2-2023-11-2

---
+++ Advanced-Programming/AP_Ex/MathFormula2/MathFormula2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+namespace MathFormula
+{
+    class Test
+    {
+        public static void Main()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            double Result = Math.Sqrt(Math.Pow(Math.Abs(x-y),Math.Abs(y)));
+            Console.WriteLine(Result);
+
+
+        }
+    }
+}
+
