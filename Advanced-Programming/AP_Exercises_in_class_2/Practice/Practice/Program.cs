ac31ab8a03e13b35d8e3364c8b0c6943732eaf2f
sinanazemm <sinanazemm@gmail.com>
2023-11-23 22:29:33 +0330

    update

---
+++ Advanced-Programming/AP_Exercises_in_class_2/Practice/Practice/Program.cs
@@ -0,0 +1,39 @@
+﻿// ba estefadeh az saghtar method add az karbar begirad on ra bar mabnayeh 2 borde va da method asli chap kon
+
+using System;
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Reverse(ToTwoDecimal(number)));
+            Console.ReadKey();
+
+
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        static string ToTwoDecimal(int num) {
+            string binarybox = "";
+  
+            while (num > 0)
+            {
+                int remainder = num % 2;
+                binarybox += remainder;
+                num /= 2;
+            }
+            return binarybox;
+
+    }
+        }
+        
+
+
+
+    }
---
+++ Advanced-Programming/AP_Exercises_in_class_2/Practice1/Practice1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] inputs = new string[3] { "L", "W", "H" };
+            double[] mesurements_input = new double[3];
+            
+            for (int i = 0; i < inputs.Length; i++) {
+                Console.WriteLine(inputs[i]);
+                mesurements_input[i] = Convert.ToDouble(Console.ReadLine());
+
+
+            }
+            Console.WriteLine(CalculateVolume(mesurements_input[0], mesurements_input[1], mesurements_input[2]));
+        }
+        static double CalculateVolume(double num1, double num2, double num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+    }
+}
