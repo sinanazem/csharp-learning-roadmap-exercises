262fa0bbcdd78e50bf271a1d00a2841ee642ac63
sinanazemm <sinanazemm@gmail.com>
2023-11-23 05:50:59 +0330

    update

---
+++ Advanced-Programming/AP_Exercises_in_class_1/MathFormula3/MathFormula3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace MathFormula3
+{
+    class Test
+    {
+        public static void Main()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            double Result = Math.Sqrt(Math.Abs(x) + Math.Sqrt(Math.Abs(x) + Math.Sqrt(Math.Abs(x))));
+            Console.WriteLine(Result);
+          
+        }
+    }
+}
