8c125f0c52f7a47bc4235ecb5d7949b1508bb2f7
sinanazem <sinanaazem@gmail.com>
2024-01-12 08:00:45 +0330

    add Circle OOP C# App

---
+++ Advanced-Programming/AP_1402_2_5/CircleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(3);
+            Console.WriteLine("Masahat is: " + circle.Masahat());
+            Console.WriteLine("Mohit is: " + circle.Mohit());
+        }
+
+    }
+
+    public class Circle
+    {
+        public double pi = 3.14;
+        private double _r;
+
+        public double R
+        {
+            get { return _r; }
+            set { _r = value; }
+        }
+
+        public Circle() { }
+        public Circle(double r)
+        {
+            _r = r;
+        }
+
+        public double Mohit()
+        {
+            return 2 * _r * pi;
+        }
+
+        public double Masahat()
+        {
+            return _r * _r * pi;
+        }
+    }
+}
