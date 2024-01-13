0b9f60d69ed45b32c9a60b93a65c896129936c43
sinanazem <sinanaazem@gmail.com>
2024-01-13 13:31:46 +0330

    add GravityDistanceFormula OOP C# App

---
+++ Advanced-Programming/AP_1402_2_6/GravityApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Time(t) for Caculate Distance Gravity formula: ");
+            double time = Convert.ToDouble(Console.ReadLine());
+
+            GravityClass obj = new GravityClass(time);
+            Console.WriteLine("Distance: " + obj.GetDistance());
+
+        }
+    }
+
+    class GravityClass
+    {
+        private double _g = 9.8;
+        private double _t;
+
+        public double T
+        {
+            get { return _t; }
+            set {
+                if (_t > 0)
+                {
+                    _t = value;
+                }
+            }
+        }
+        public GravityClass() { }
+        public GravityClass(double t)
+        {
+            _t = t;
+        }
+
+        public double GetDistance()
+        {
+            return 0.5 * _g * _t;
+        }
+    }
+
+}
