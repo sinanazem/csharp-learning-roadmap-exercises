ce6d1b0d7c2dd4a10bb4cfab7296df80dd2d5831
sinanazem <sinanaazem@gmail.com>
2024-01-28 05:46:34 +0330

    add Discount project code

---
+++ Advanced-Programming/AP_1402_2_Final_Projects/Discount/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+class DiscountCodeValidator
+{
+    private string validCode;
+
+    public string ValidCode
+    {
+        get { return validCode; }
+        set { validCode = value; }
+    }
+    
+    public DiscountCodeValidator() { }
+    public DiscountCodeValidator(string validCodeInput)
+    {
+        validCode = validCodeInput;
+    }
+
+    public bool IsValidDiscountCode(string code)
+    {
+        HashSet<char> codeSet = new HashSet<char>(code);
+        HashSet<char> validSet = new HashSet<char>(validCode);
+        return codeSet.SetEquals(validSet);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        int n = Convert.ToInt32(Console.ReadLine());
+        string t = Console.ReadLine().Trim();
+
+        DiscountCodeValidator validator = new DiscountCodeValidator(t);
+
+        string[] codes = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            codes[i] = Console.ReadLine().Trim();
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            bool isValid = validator.IsValidDiscountCode(codes[i]);
+            
+            if (isValid)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            
+        }
+    }
+}
